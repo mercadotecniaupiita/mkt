66a1c684a2a8c049344b64aebb49728bd6c35c43
lmoralesv96 <lmoralesv1100@alumno.ipn.mx>
2021-07-24 00:30:19 -0500

    Se borran los sliders para revision

--- enviar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$destino = "luismoralesv.96@gmail.com";
-$nombre = $_Post["Nombre"];
-$correo = $_Post["Correo"];
-$mensaje = $_Post["mensaje"];
-$contenido = "Nombre: " . $nombre . "\nCorreo: " , $correo;
-mail($destino,"Contacto", $contenido);
-header("Location: contacto.html");
-?>
